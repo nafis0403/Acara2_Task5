58fcf13a6ac5215ea3a86d9c4bf150e31160825d
nafis0403 <151050078+nafis0403@users.noreply.github.com>
2024-03-03 15:47:17 +0700

    Add files via upload

---
+++ Button2.cs
@@ -0,0 +1,29 @@
+﻿using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Layouts;
+using ArcGIS.Desktop.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadSecondAddin
+{
+    internal class Button2 : Button
+    {
+        protected override void OnClick()
+        {
+            ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show("Add-in 2's Button here!", "Info");
+        }
+    }
+}
